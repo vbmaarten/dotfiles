7f554ac4a5913fa2bee5997669e7ed95ec5f0dc6
Maarten van Beek <myemail>
2023-09-27 15:37:44 +0200

    Cleaned plugin list

--- vim/.config/nvim/lua/plugins/custom_setup.lua
+++ vim/.config/nvim/lua/plugins/custom_setup.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "lvim-tech/lvim-colorscheme",
-    init = function()
-      require("lvim-colorscheme").setup()
-    end,
-    priority = 1000,
-  },
-  {
     "catppuccin/nvim",
     lazy = false,
     name = "catoppuccin",
@@ -42,28 +35,9 @@ return {
   },
   { import = "lazyvim.plugins.extras.util.project" },
   {
-    "s1n7ax/nvim-window-picker",
-    name = "window-picker",
-    event = "VeryLazy",
-    version = "2.*",
-    config = function()
-      require("window-picker").setup()
-    end,
-  },
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    config = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-  {
-    "alpha2phi/lp.nvim",
-    lazy = false,
   },
   -- add pyright to lspconfig
   {
@@ -156,37 +130,9 @@ return {
       },
     },
   },
-  {
-    "jubnzv/mdeval.nvim",
-    lazy = false,
-  },
-
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
   { import = "lazyvim.plugins.extras.lang.typescript" },
-
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
 
   -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
   -- would overwrite `ensure_installed` with the new value.
@@ -200,16 +146,6 @@ return {
         "typescript",
       })
     end,
-  },
-  {
-    "jim-fx/sudoku.nvim",
-    cmd = "Sudoku",
-    config = function()
-      require("sudoku").setup({})
-    end,
-  },
-  {
-    "alec-gibson/nvim-tetris",
   },
   {
     "petertriho/nvim-scrollbar",
@@ -232,64 +168,6 @@ return {
           ale = false, -- Requires ALE
         },
       })
-    end,
-  },
-  {
-    "dimaportenko/telescope-simulators.nvim",
-    config = function()
-      require("simulators").setup({
-        android_emulator = false,
-        apple_simulator = true,
-      })
-    end,
-  },
-  { "jbyuki/quickmath.nvim" },
-  {
-    "mxsdev/nvim-dap-vscode-js",
-    config = function()
-      require("dap-vscode-js").setup({
-        -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-        -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-        debugger_path = "/Users/maartenvanbeek/Downloads/vscode-js-debug",
-        -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { "pwa-node", "pwa-chrome", "node-terminal" }, -- which adapters to register in nvim-dap
-        -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-        -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-        -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-      })
-      for _, language in ipairs({ "typescript", "javascript" }) do
-        require("dap").configurations[language] = {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Debug Jest Tests",
-            -- trace = true, -- include debugger info
-            runtimeExecutable = "node",
-            runtimeArgs = {
-              "./node_modules/jest/bin/jest.js",
-              "--runInBand",
-            },
-            rootPath = "${workspaceFolder}",
-            cwd = "${workspaceFolder}",
-            console = "integratedTerminal",
-            internalConsoleOptions = "neverOpen",
-          },
-        }
-      end
     end,
   },
   {
@@ -323,23 +201,6 @@ return {
       notification = false,
       disabled_keys = {},
     },
-  },
-  {
-    "psebaraj/tomatotimer.nvim",
-    requires = "MunifTanjim/nui.nvim",
-    config = function()
-      require("tomatotimer").setup({
-        time_work = 20,
-        time_break_short = 5,
-        time_break_long = 20,
-        timers_to_long_break = 4,
-      })
-      require("lualine").setup({
-        sections = {
-          lualine_c = { "filename", require("tomatotimer").statusline },
-        },
-      })
-    end,
   },
   {
     "LazyVim/LazyVim",
